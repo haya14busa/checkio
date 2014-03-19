9130b5547dd5851baf442411c5c0b5e53e3a1748
haya14busa <hayabusa1419@gmail.com>
2014-03-20 03:38:18 +0900

    Solve the most numbers

---
+++ Library2.0/04_the_most_numbers.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# FILE: Library2.0/04_the_most_numbers.py
+# AUTHOR: haya14busa
+# License: MIT license
+#
+#     Permission is hereby granted, free of charge, to any person obtaining
+#     a copy of this software and associated documentation files (the
+#     "Software"), to deal in the Software without restriction, including
+#     without limitation the rights to use, copy, modify, merge, publish,
+#     distribute, sublicense, and/or sell copies of the Software, and to
+#     permit persons to whom the Software is furnished to do so, subject to
+#     the following conditions:
+#
+#     The above copyright notice and this permission notice shall be included
+#     in all copies or substantial portions of the Software.
+#
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#     OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+#     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+#     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#     SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#=============================================================================
+
+
+def diff_max_min(*args):
+    """
+    The diff between max and min.
+    """
+    return max(args) - min(args) if args else 0
+
+
+def checkio(*args):
+    return diff_max_min(*args)
+
+
+if __name__ == '__main__':
+    def almost_equal(checked, correct, significant_digits):
+        precision = 0.1 ** significant_digits
+        return correct - precision < checked < correct + precision
+
+    assert almost_equal(checkio(1, 2, 3), 2, 3), "3-1=2"
+    assert almost_equal(checkio(5, -5), 10, 3), "5-(-5)=10"
+    assert almost_equal(checkio(10.2, -2.2, 0, 1.1, 0.5), 12.4, 3), \
+        "10.2-(-2.2)=12.4"
+    assert almost_equal(checkio(), 0, 3), "Empty"
+    print('All done')
